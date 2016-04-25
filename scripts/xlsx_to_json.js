161cf88bd2466264074753fffce5f5cb837dfcac
Mats Rörbecker <mats@rorbecker.com>
2016-04-25 09:12:36 +0200

    Adds data for 2016

--- scripts/xlsx_to_json.js
+++ scripts/xlsx_to_json.js
@@ -9,18 +9,18 @@ var newData = [];
 for (var i = 0; i < data.length; i += 3) {
     var obj = {};
     obj.municipality = data[i].municipality.trim();
-    obj.unemployedWomen2014 = data[i].unemployed2014;
     obj.unemployedWomen2015 = data[i].unemployed2015;
-    obj.unemployedMen2014 = data[i + 1].unemployed2014;
+    obj.unemployedWomen2016 = data[i].unemployed2016;
     obj.unemployedMen2015 = data[i + 1].unemployed2015;
-    obj.unemployed2014 = data[i + 2].unemployed2014;
+    obj.unemployedMen2016 = data[i + 1].unemployed2016;
     obj.unemployed2015 = data[i + 2].unemployed2015;
-    obj.change = (obj.unemployed2015 - obj.unemployed2014).toFixed(1);
+    obj.unemployed2016 = data[i + 2].unemployed2016;
+    obj.change = (obj.unemployed2016 - obj.unemployed2015).toFixed(1);
     newData.push(obj);
 };
 
 var compare = function (obj1, obj2) {
-    return obj1.unemployed2015 - obj2.unemployed2015;
+    return obj1.unemployed2016 - obj2.unemployed2016;
 };
 
 // Objektet med genomsnitt för alla kommuner ska inte vara med i rankningen
